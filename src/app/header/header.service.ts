9dd9bf363d3c8e207cf4a651ad57cd6f9c375dd3
Aitor Ibañez <aibanez@irontec.com>
2017-01-08 17:01:06 +0100

    Añadido servicio para header

---
+++ src/app/header/header.service.ts
@@ -0,0 +1,16 @@
+import { Observable, Observer } from 'rxjs/Rx';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeaderService {
+	public getHeader(): Observable<any[]> {
+		return new Observable<any[]>((obs: Observer<any[]>) => {
+			let data: any[] = [];
+
+            data = ['registro', 'login'];
+			obs.next(data);
+			obs.complete();
+
+		});
+	}
+}
